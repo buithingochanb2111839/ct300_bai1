7c21e3ecfada3bbbb969bf4a9228e1be8dd6fe4e
buithingochanb2111839 <hanb2111839@student.ctu.edu.vn>
2024-09-20 16:12:31 +0700

    Added for

---
+++ for_BT4.java
@@ -0,0 +1,8 @@
+public class for_BT4 {
+    public static void main(String[] args) {
+        // Vòng lặp for để in các số từ 1 đến 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
